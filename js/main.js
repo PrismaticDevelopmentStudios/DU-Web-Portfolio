f0ac747e97da247562ad5c60d2154ce3e4426d46
Matthew Brignola <matthewbrignola@ymail.com>
2021-10-23 20:10:59 -0600

    sections font edit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 function hi() {
     alert('Hi!');
+    console.log('hi');
+    window.scrollTo(0, 0);
 }
 
 function toTop() {
-    window.scrollTo(0, 0)
+    window.scrollTo(0, 0);
+    console.log('BTT')
 }
