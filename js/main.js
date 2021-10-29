4a8af98578be1e1a8f89ceb27946923a20fe70df
Matthew Brignola <matthewbrignola@ymail.com>
2021-10-28 18:18:13 -0600

    navbar icon padding fixed

--- js/main.js
+++ js/main.js
@@ -8,3 +8,8 @@ function toTop() {
     window.scrollTo(0, 0);
     console.log('BTT')
 }
+
+function closeModal() {
+    document.getElementById('modal').style.display = "none";
+    console.log('close');
+}
